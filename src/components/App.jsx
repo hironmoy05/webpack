1c8d778368906931a6a4b45ba97c2f3275b29fff
Hironmoy <hironmoy@gmail.com>
2021-03-08 18:51:28 +0530

    add images in react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
 import "../styles/index.scss";
 import Recipe from "./Recipie";
+import sword from "../images/swc-sword.png";
+import swordSvg from "../images/sword.svg";
 
 const App = () => {
   return(
     <>
-      <section className="hero">
+      <section className="hero"></section>
         <main>
           <section>
           <h1>Oh Hai, React</h1>
           </section>
-        </main>
-      </section>
+          <img src={sword} alt="sword" width="250" />
+          <img src={swordSvg} alt="sword" width="250" />
       <Recipe />
+        </main>
     </>
   )
 }
